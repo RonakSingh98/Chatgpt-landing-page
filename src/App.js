4c572a58a6cbd2fd560fcda935f5ae9cc875682b
RonakSingh98 <129096563+RonakSingh98@users.noreply.github.com>
2023-05-01 20:43:34 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import {Blog,Features,Footer,Possibility,WhatGPT3,Header} from './containers';
-import {Article,Brand,CTA,Feature,Navbar} from './components';
-import './App.css';
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar/>
-        <Header/>
-     </div>
-        <Brand/>
-        <WhatGPT3/>
-        <Features/>
-        <Possibility/>
-        <CTA/>
-        <Blog/>
-        <Footer/>
-    </div>
-  )
-}
+import React from 'react';
 
-export default App
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="https://openai.com/">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
