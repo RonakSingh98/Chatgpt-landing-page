2e694cbd65e378dab12cba9fd5bd7dcde68da1d4
Ankur <canibecomeacoder@gmail.com>
2023-05-04 23:41:19 +0530

    final vercel

--- server/connect_db/connect_db.js
+++ server/connect_db/connect_db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const main = async () => {
-    await mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://vercel:WIJIUcHU8X7BQPLc@live-project-01-cluster.on56otu.mongodb.net/?retryWrites=true&w=majority",{
+    await mongoose.connect(process.env.MONGODB_URL || "http://127.0.0.1:27017",{
     autoIndex: true});
     console.log("Mongoose connected");
 }
